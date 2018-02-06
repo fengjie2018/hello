1f4d79344bec4a1072f0034230021596f73a2dde
fengjie <1320627372@qq.com>
2018-02-06 09:04:04 +0800

    创建
    fengjie在2018年2月6日创建了三个文件，分别是README.md hello.java
    helloworld.cpp

---
+++ hello.java
@@ -0,0 +1,7 @@
+/**
+ *@如你所见
+ */
+public class hello{
+    public static void main(String[] args){
+        System.out.println("hello world!");
+}
---
+++ helloworld.cpp
@@ -0,0 +1,11 @@
+//helloworld.cpp
+//国际惯例
+//fengjie2018@qq.com
+//2018年2月5日
+
+#include<iostream>
+
+int main(){
+    std::cout<<"hello,world!\n";
+    return 0;
+}
